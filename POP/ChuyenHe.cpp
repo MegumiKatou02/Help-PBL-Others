a3f54b23e4f892117de31c59f9264f1cd390c4a8
ledinhchinh0212 <khongbachofficial@gmail.com>
2024-05-10 21:16:41 +0700

    complete Chuyen sang he nhi phan

--- POP/ChuyenHe.cpp
+++ POP/ChuyenHe.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+string chuyenSangNhiPhan(int n)
+{
+    if(n == 0)
+    {
+        return "0";
+    }
+    string s = "";
+    while(n > 0)
+    {
+        int du = n % 2;
+        s += to_string(du);
+        n /= 2; 
+    }
+    return s;
+}
+
 int main()
 {
-
+    int n;
+    do
+    {
+        cin  >> n;
+    } while (n < 0);
+    
+    cout << chuyenSangNhiPhan(n);
     return 0;
 }
