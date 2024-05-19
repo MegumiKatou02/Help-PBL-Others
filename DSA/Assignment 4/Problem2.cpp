9b639fa337259678d8f76ffcefde40c01da32657
ledinhchinh0212 <khongbachofficial@gmail.com>
2024-05-19 08:02:44 +0700

    complete

---
+++ DSA/Assignment 4/Problem2.cpp
@@ -0,0 +1,99 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+	
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  	
+  	void preOrder(Node *root) {
+		
+      	if( root == NULL )
+          	return;
+      
+      	std::cout << root->data << " ";
+      	
+      	preOrder(root->left);
+      	preOrder(root->right);
+	}
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    Node * insert(Node * root, int data) {
+Node *node = new Node(data);
+        Node *current = root;
+
+        if(root == nullptr) return node;
+
+        Node *parent = nullptr;
+
+        while(current != nullptr)
+        {
+            parent = current;
+            if(data <= current->data)
+            {
+                current = current->left;
+            }
+            else
+            {
+                current = current->right;
+            }
+        }
+        if(data <= parent->data)
+        {
+            parent->left = node;
+        }
+        else 
+        {
+            parent->right = node;
+        }
+        return root;
+    }
+}; 
+
+int main() {
+    
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    myTree.preOrder(root);
+
+    return 0;
+}
