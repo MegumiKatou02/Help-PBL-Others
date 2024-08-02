f527154f685a1061789be21e313525b1f3c4b709
ledinhchinh0212 <khongbachofficial@gmail.com>
2024-08-02 14:35:27 +0700

    complete

---
+++ DSA/LeetCode/283.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector<int> v;
+        int zeros = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] != 0)
+            {
+                v.push_back(nums[i]);
+            }
+            else zeros++;
+        }
+        for(int i = 0; i < zeros; i++)
+        {
+            v.push_back(0);
+        }
+        nums = v;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
