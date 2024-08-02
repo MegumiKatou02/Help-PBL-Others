6be1cd455e8520013b251aba8ba85e455f4c69fd
ledinhchinh0212 <khongbachofficial@gmail.com>
2024-08-02 18:42:21 +0700

    complete

---
+++ DSA/LeetCode/206.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {} 
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == nullptr) return nullptr;
+        ListNode *prev = nullptr;
+        ListNode *cur = head;
+        while(cur != nullptr)
+        {
+            ListNode *fur = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = fur;
+        }
+        head = cur;
+        return head;
+    }
+};
