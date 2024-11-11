42a6daabbf8fd3887be42fe16b7032870bc82b59
ledinhchinh0212 <khongbachofficial@gmail.com>
2024-11-11 11:42:26 +0700

    completet

--- DSA/LeetCode/SameTree.cpp
+++ DSA/LeetCode/SameTree.cpp
@@ -2,6 +2,30 @@
 
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p != nullptr && q != nullptr)
+        {
+            if(p->val != q->val) return false;
+            return isSameTree(p->left, q->left);
+            return isSameTree(p->right, q->right);
+        }
+        else if(p != nullptr && q == nullptr) return false;
+        else if(p == nullptr && q != nullptr) return false;
+        else return true;
+    }
+};
+
 int main()
 {
 
